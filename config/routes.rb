f47f95131af263c3732fab536cae879f7ae09678
masatoshi.murakami <masatoshi.murakami@coconala.com>
2020-07-24 14:21:10 +0900

    letter_openerをマウント

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   namespace :api, format: 'json' do
     resources :tasks, only: [:index, :create, :update]
   end
+
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end

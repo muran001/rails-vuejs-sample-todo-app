ba4f821f0179ac3afd93b5b090fa046565e5727e
masatoshi.murakami <masatoshi.murakami@coconala.com>
2020-07-24 15:32:35 +0900

    devise ログインAPI

--- app/controllers/auth/registrations_controller.rb
+++ app/controllers/auth/registrations_controller.rb
@@ -1,4 +1,5 @@
 class Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+  skip_before_action :verify_authenticity_token, if: :devise_controller?
   private
   def sign_up_params
     params.permit(:name, :email, :password, :password_confirmation)

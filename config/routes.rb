39b38b1cb6fc42894c37f4b8cb106d73e7ae24c7
masatoshi.murakami <masatoshi.murakami@coconala.com>
2020-07-24 14:41:05 +0900

    devise用設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
   root to: 'home#index'
   get 'home/index'
   namespace :api, format: 'json' do
     resources :tasks, only: [:index, :create, :update]
   end
 
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+    registrations: 'auth/registrations'
+  }
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
